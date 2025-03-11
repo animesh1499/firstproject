a65a9cc06061e321f03026222a375e8c206a7b1e
animesh1499 <animeshjain1499@gmail.com>
2025-03-11 14:08:17 +0530

    Added Oauth2 with JWT.

--- src/main/java/org/example/firstproject/projectcontrollers/UsersController.java
+++ src/main/java/org/example/firstproject/projectcontrollers/UsersController.java
@@ -1,14 +1,13 @@
 package org.example.firstproject.projectcontrollers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.example.firstproject.modals.Users;
 import org.example.firstproject.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -29,19 +28,24 @@ public class UsersController {
         return userServices.register(user);
     }
 
-    @PostMapping(value = "/login")
+    @PostMapping(value = "/user/login")
     public String login(@RequestBody Users user){
         return userServices.verify(user);
     }
 
     @PostMapping(value = "/user/logout")
-    public String logout(@RequestHeader("Authorization") String authHeader){
+    public String logout(@RequestHeader("Authorization") String authHeader, HttpServletRequest request){
         System.out.println("Inside logout!");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
             System.out.println("This is token in logout " + token);
             // Set expiry time equal to token expiration (30 min in this case)
             redisTemplate.opsForValue().set(BLACKLIST_PREFIX + token, "true", 30, TimeUnit.MINUTES);
+            HttpSession session = request.getSession(false); // Get existing session
+            if (session != null) {
+                System.out.println("Session : " + session.getId());
+                session.invalidate(); // Invalidate session
+            }
 
             return "Logged out successfully!";
         }
